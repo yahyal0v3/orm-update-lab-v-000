7cdda02ad40b9e58a3d1aaa5a06a2ef17a68f8bf
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-26 04:23:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,44 @@
+require 'pry'
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade, :id
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = nil
+  end
 
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)")
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    if self.id == nil
+      DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    binding.pry
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    student = Student.new(name, grade, id)
+  end
 
 end
